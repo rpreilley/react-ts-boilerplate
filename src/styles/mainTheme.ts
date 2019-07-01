e07ce642a2785aab407773ba3afe5c0c9793ba09
Bob Reilley <rreilley@vinformatix.com>
2019-07-01 14:53:54 -0500

    working through ts errors for scss plus more

--- src/styles/mainTheme.ts
+++ src/styles/mainTheme.ts
@@ -1,15 +1,17 @@
-import * as styles from './globalStyles.scss';
+import styles from './globalStyles.scss'
+import { createMuiTheme } from "@material-ui/core";
 
 // Main Theme styles are now directly linked to global scss variables via sass :export directive
 
-const mainTheme = {
+const mainTheme = createMuiTheme({
   primary: styles.primary,
   secondary: styles.secondary,
   accent: styles.accent,
   error: styles.error,
   info: styles.info,
   success: styles.success,
-  warning: styles.warning
-}
+  warning: styles.warning,
+  title: styles.title
+});
 
 export default mainTheme;
