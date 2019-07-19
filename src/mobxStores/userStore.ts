194bfcb15f47d440678cf0174ed3143c3582202b
Bob Reilley <rpreilley@gmail.com>
2019-07-19 10:02:03 -0500

    more user store notes

--- src/mobxStores/userStore.ts
+++ src/mobxStores/userStore.ts
@@ -1,4 +1,5 @@
 import { observable, action } from 'mobx';
+import ApiService from '../services/ApiService';
 
 export default class UserStore {
   @observable userId: Number;
@@ -17,7 +18,33 @@ export default class UserStore {
 
   @action
   authenticateUser(data: Object) {
+    // Url needed for authentication endpoint
+    const url = '';
 
+    // Map out data from authentication form being passed in to send to endpoint
+
+    // Pass any additional options, like headers
+    const options = {
+
+    };
+
+    ApiService.post(url, data, options)
+      .then((response) => {
+        // If successful response, set returned user values as store values
+        console.log(response);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+  }
+
+  @action
+  clearUserSession() {
+    this.userId = null!;
+    this.firstName = '';
+    this.lastName = '';
+    this.emailAddress = '';
+    this.userSession = {};
   }
 
 }
